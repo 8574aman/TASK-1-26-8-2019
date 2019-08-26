5ef003edb86b5b04cc1ef5af15b5ea7dcb8fd3ae
TSTARKS <8574aman@gmail.com>
2019-08-26 20:36:11 +0530

    My first commit

---
+++ src/CalculateArea.java
@@ -0,0 +1,16 @@
+import com.shapes.myFirstApi2.*;
+public class CalculateArea {
+
+	public static void main(String[] args) {
+		
+		//Using Circle class 
+		Circle c1 = new Circle ();
+		c1.getCircleArea(20.5);
+		
+		
+		//Using Square class
+		Square s1 = new Square();
+		s1.getSquareArea(10);
+	}
+
+}
